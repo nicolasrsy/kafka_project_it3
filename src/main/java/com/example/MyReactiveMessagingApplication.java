60d281b15334e89f58e59ea7def8155faca15d0d
Katia Hammache <hammache.katia1@gmail.com>
2023-12-13 14:21:04 +0100

    init project

--- src/main/java/com/example/MyReactiveMessagingApplication.java
+++ src/main/java/com/example/MyReactiveMessagingApplication.java
@@ -9,35 +9,66 @@ import jakarta.inject.Inject;
 
 import java.util.stream.Stream;
 
+import static java.lang.Thread.sleep;
+
 @ApplicationScoped
 public class MyReactiveMessagingApplication {
 
+    private static int carCount = 0;
+
     @Inject
-    @Channel("words-out")
-    Emitter<String> emitter;
+    @Channel("Selling")
+    Emitter<String> sellingEmitter;
+
+    @Inject
+    @Channel("Producer")
+    Emitter<String> producerEmitter;
+
 
     /**
      * Sends message to the "words-out" channel, can be used from a JAX-RS resource or any bean of your application.
      * Messages are sent to the broker.
      **/
     void onStart(@Observes StartupEvent ev) {
-        Stream.of("Hello", "with", "SmallRye", "reactive", "message").forEach(string -> emitter.send(string));
+        Stream.of("M", "I", "A", "G", "E").forEach(string -> sellingEmitter.send(string));
     }
 
     /**
      * Consume the message from the "words-in" channel, uppercase it and send it to the uppercase channel.
      * Messages come from the broker.
      **/
-    @Incoming("words-in")
-    @Outgoing("uppercase")
-    public Message<String> toUpperCase(Message<String> message) {
-        return message.withPayload(message.getPayload().toUpperCase());
+    @Incoming("Producer")
+    @Outgoing("Delivery")
+    public Message<String> produceCar(Message<String> model) throws InterruptedException {
+        // Simulate production time based on model
+        if (model.equals("M")) {
+            sleep(10000);
+        } else if (model.equals("I")) {
+            sleep(2000);
+        } else if (model.equals("A")) {
+            sleep(3000);
+        } else if (model.equals("G")) {
+            sleep(4000);
+        } else if (model.equals("E")) {
+            sleep(7000);
+        }
+
+        System.out.println("Produced car: " + model);
+        // Logic to count produced cars and trigger delivery
+        // Production logic as above
+        carCount++;
+        if (carCount >= 10) {
+            producerEmitter.send("Delivery ready");
+            carCount = 0;
+        }
+        return null;
     }
 
     /**
      * Consume the uppercase channel (in-memory) and print the messages.
      **/
-    @Incoming("uppercase")
+    @Incoming("Delivery")
+
     public void sink(String word) {
         System.out.println(">> " + word);
     }
